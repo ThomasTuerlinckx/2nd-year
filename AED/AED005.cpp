abacb6b76504905c8d9ee1dfe0dfc2310afa745e
ThomasTuerlinckx <up202403993@up.pt>
2025-10-02 17:34:51 +0100

    TENS SO MAIS 4 IFS E TA BOM, AKA SE É MAIOR QUE MIDDLE OU N

--- AED/AED005.cpp
+++ AED/AED005.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <complex>
 #include <iostream>
 using namespace std;
 
@@ -9,12 +10,30 @@ bool real_sqrt(int a) {
   return root * root == a;
 }
 
+bool isPair(int a) {
+  if (a % 2 == 0) {
+    return true;
+  } else
+    return false;
+}
+
+int YSquares(int num) {
+  if (isPair(num)) {
+    int y = (sqrt(num) - 1) / 2;
+    return y;
+  } else {
+    int y = sqrt(num) / 2;
+    y = -y;
+    return y;
+  }
+}
+
 int main() {
   int num;
   cin >> num;
   int x, y;
   if (real_sqrt(num)) {
-    if (num % 2 != 0) {
+    if (isPair(num)) {
       x = sqrt(num) - 1;
       x = x / 2;
       y = x;
@@ -23,6 +42,37 @@ int main() {
       y = sqrt(num) / 2;
       y = -y;
       x = y + 1;
+    }
+  }
+
+  else {
+    int lower = num;
+    int upper = num;
+    bool up = false;
+    bool down = false;
+    while (up == false or down == false) {
+      if (real_sqrt(lower) != false) {
+        lower--;
+      } else
+        down = true;
+
+      if (real_sqrt(upper)) {
+        upper--;
+      } else
+        up = true;
+    }
+
+    int Xmiddle, Ymiddle;
+    if (isPair(upper)) {
+      int Yupper = YSquares(upper);
+      Xmiddle = -Yupper;
+      Ymiddle = Yupper;
+
+      if (!isPair(upper)) {
+        int Yupper = YSquares(upper);
+        Xmiddle = -Yupper;
+        Ymiddle = Yupper;
+      }
     }
   }
 
